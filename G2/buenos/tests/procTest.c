6fcf3f9f386fa9304859d76cbabc9622d99b6da9
sebastian <sebastian@momentos.dk>
2015-02-23 22:01:22 +0100

    still working on test

--- G2/buenos/tests/procTest.c
+++ G2/buenos/tests/procTest.c
@@ -1,12 +1,12 @@
 #include "tests/lib.h"
 
-//void print(char* string);
+void print(char* string);
 int main(void)
 {
 
     int child1;
     int child2;
-    //print("234567890");
+    print("234567890");
     child1 = syscall_exec("[disk]test1");
     //print("starting process test2");
     child2 = syscall_exec("[disk]test1");
@@ -15,22 +15,16 @@ int main(void)
     //print("joining test2 as child2");
     syscall_join(child2);
     //print("exit child1");
-    syscall_exit(child1);
-    //print("exit child2");
-    syscall_exit(child2);
-    //print("calling syscal halt");
 
     syscall_halt();
     return 0;
 
 }
 
-/*void print(char *string){
-        syscall_write(1, "s", 1);
-        syscall_write(1, "s", 1);
-        syscall_write(1, "s", 1);
-
-        syscall_write(1, string, sizeof(string));
+void print(char* string){
+    unsigned int i;
+    for(i = 0; i < sizeof(string); i++) {
+        syscall_write(1, string[i], 1);
     }
+}
 
-*/
