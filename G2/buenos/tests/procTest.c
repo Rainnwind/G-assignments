326c191e8af377229d218a2e41ee4c2169ba5cf2
sebastian <sebastian@momentos.dk>
2015-02-23 23:20:33 +0100

    changed a litle in test

--- G2/buenos/tests/procTest.c
+++ G2/buenos/tests/procTest.c
@@ -9,15 +9,16 @@ int main(void)
     printit(string, sizeof(string));
     child1 = syscall_exec("[disk]test1");
 
-    char string2[] = "start second process of test1\n";
-    printit(string2, sizeof(string2));
-
-    child2 = syscall_exec("[disk]test1");
-
     char string3[] = "join first instance of test1\n";
     printit(string3, sizeof(string3));
 
     syscall_join(child1);
+
+
+    char string2[] = "start second process of test1\n";
+    printit(string2, sizeof(string2));
+
+    child2 = syscall_exec("[disk]test1");
 
     char string4[] = "join second instance of test1\n";
     printit(string4, sizeof(string4));
